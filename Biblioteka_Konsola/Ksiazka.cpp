ca13d3580467082d4dbbe6eaba3aa4e2e75c4f10
KwasnikMaciej <kwasnik.maciej95@gmail.com>
2015-03-29 21:51:00 +0200

    Przeróbka WyswietlKsiazke na czytelniejsza wersje

--- Biblioteka_Konsola/Ksiazka.cpp
+++ Biblioteka_Konsola/Ksiazka.cpp
@@ -13,16 +13,16 @@ string Ksiazka::getPierwszyRozdzial() { return this->PierwszyRozdz; }
 
 void Ksiazka::WyswietlKsiazke()
 {
-	cout << "Tytul: "; cout << this->getTytul();
-	cout << "\nAutor: "; cout << this->getAutor(); cout << endl << "       ";
-	cout << this->Wydawnictwo;	cout << ", ";
-	cout << this->RokWydania; cout << "\n";
+	cout << "Tytul: " << this->getTytul() << endl;
+	cout << "Autor: " << this->getAutor() << endl;
+	cout << this->Wydawnictwo << ", ";
+	cout << this->RokWydania << endl;
 
-		cout << "\nCzy chcesz wyswietlic jej 1. rozdzial? 1 - tak, 0 - nie: ";
-		short int dec = 0;
-		cin >> dec;
-		if (dec!=0) this->WyswietlRozdzial();
-		cout << endl << endl << endl;
+	cout << "\nCzy chcesz wyswietlic jej 1. rozdzial? 1 - tak, 0 - nie: ";
+	short int dec = 0;
+	cin >> dec;
+	if (dec!=0) this->WyswietlRozdzial();
+	cout << endl << endl << endl;
 }
 
 void Ksiazka::WyswietlRozdzial()
@@ -73,6 +73,7 @@ Ksiazka::Ksiazka(char _ISBN[15], string _Tytul, string _Autor, int _RokWyd, stri
 		this->ListaEgz[i] = Egzemplarz();
 	}
 }
+
 Ksiazka::Ksiazka(char _ISBN[15], string _Tytul, string _Autor, int _RokWyd, string _Wydawnictwo,
 	string _Opis, string _Typ, string _PierwszyRozdz){
 	for (int i = 0; i < 15; i++)
